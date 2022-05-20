5c0a3fe2e416a0e5b6e43d90af94e42e25a30559
Matěj Zábský <mzabsky@gmail.com>
2022-05-20 12:46:57 +0200

    Initial commit

---
+++ SourceGeneratorImpl/Class1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace SourceGeneratorImpl
+{
+    [Generator]
+    public class SourceGenerator: ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            string rootNamespace;
+            if (!context.AnalyzerConfigOptions.GlobalOptions.TryGetValue("build_property.RootNamespace", out rootNamespace))
+            {
+                rootNamespace = "NotFound";
+            }
+
+            var masterSource = $"public static class GeneratedClass {{ public static string {rootNamespace} = \"Hello\" }}";
+            context.AddSource("Generated.cs", masterSource);
+        }
+    }
+}
---
+++ SourceGeneratorTest/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace SourceGeneratorTest;
+
+public class Program
+{
+    public static void Main()
+    {
+        var a = GeneratedClass.NotFound;
+        var b = GeneratedClass.SourceGeneratorTest;
+        
+        Console.WriteLine($"{a} {b}");
+    }
+}
